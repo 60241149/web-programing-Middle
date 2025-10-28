4c8a1c0222e81d6703bd4d272756b6c8df6c9245
60241149 <smj102789@gmail.com>
2025-10-29 03:58:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 press.addEventListener("click", () => {
-  console.log("PRESS clicked"); // 개발자도구에서 눌림 확인
+  stage.classList.remove("to-about");
+  console.log("PRESS clicked");
   stage.classList.add("dim-now");
   setTimeout(() => {
     stage.classList.add("show-next");
@@ -9,14 +10,22 @@ press.addEventListener("click", () => {
   }, 500);
 });
 
-const soldier = document.querySelector(".soldier-img");
-if (soldier) {
-  soldier.addEventListener("mouseenter", startAutoFire);
-  soldier.addEventListener("mouseleave", stopAutoFire);
-}
-
 document
   .querySelector(".top-menu .menu-item:first-child")
   ?.addEventListener("click", () => {
-    stage.classList.remove("to-about"); // ← 이게 있어야 PREORDER가 다시 나타남
+    stage.classList.remove("to-about");
   });
+
+const bgm = document.getElementById("bgm");
+
+press.addEventListener("click", () => {
+  bgm.play().catch((err) => console.log("Autoplay blocked:", err));
+
+  stage.classList.add("dim-now");
+  setTimeout(() => {
+    stage.classList.add("show-next");
+    document.querySelector(".logo-wrap").style.display = "none";
+    press.style.display = "none";
+    stage.classList.remove("dim-now");
+  }, 500);
+});
